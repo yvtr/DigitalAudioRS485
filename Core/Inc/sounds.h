fcf303ab8f031b86b927eae7b38137463b861fdf
unicod <unicod@noreply.codeberg.org>
2026-05-09 15:49:25 +0200

    Added new 16bit PCM sounds, removed old 8bit sound

---
+++ Core/Inc/sounds.h
@@ -0,0 +1,24 @@
+#ifndef __SOUNDS_H__
+#define __SOUNDS_H__
+
+#include <stdint.h>
+
+
+extern const int16_t PCM_Deutsch[];
+extern const int16_t PCM_English[];
+extern const int16_t PCM_Magyar[];
+extern const int16_t PCM_Dolmetscheranlage[];
+extern const int16_t PCM_InterpreterSystem[];
+extern const int16_t PCM_Tolmacsrendszer[];
+
+extern const uint32_t PCM_DeutschLen;
+extern const uint32_t PCM_EnglishLen;
+extern const uint32_t PCM_MagyarLen;
+extern const uint32_t PCM_DolmetscheranlageLen;
+extern const uint32_t PCM_InterpreterSystemLen;
+extern const uint32_t PCM_TolmacsrendszerLen;
+
+
+
+
+#endif
